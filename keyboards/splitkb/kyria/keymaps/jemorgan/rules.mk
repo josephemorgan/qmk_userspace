6eceb6654faa5bb52cbcc1994039f47f8869eb97
Joseph Morgan <jm@jemorgan.dev>
2026-08-03 12:43:32 -0700

    Updates to planck layout

--- keyboards/splitkb/kyria/keymaps/jemorgan/rules.mk
+++ keyboards/splitkb/kyria/keymaps/jemorgan/rules.mk
@@ -2,6 +2,7 @@ WPM_ENABLE = yes
 OLED_ENABLE = yes
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow MOUSEKEY_ENABLE = yes
 LEADER_ENABLE = yes
+MOUSEKEY_ENABLE = yes
 
 
 EXTRAKEY_ENABLE = no # Audio control and System control
