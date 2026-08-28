c158819403e1d3ff4aa82405d72442ab7e382cc5
Joseph Morgan <jm@jemorgan.dev>
2026-08-28 12:18:22 -0700

    Minor tweaks

--- keyboards/splitkb/kyria/keymaps/jemorgan/keymap.c
+++ keyboards/splitkb/kyria/keymaps/jemorgan/keymap.c
@@ -30,19 +30,19 @@ void           update_jiggler(uint16_t);
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//        ┌──────┬───┬───┬─────────────┬──────┬──────┐                                                               ┌──────┬──────┬─────────┬───┬───┬──────┐
-//        │  `   │ q │ w │      e      │  r   │  t   │                                                               │  y   │  u   │    i    │ o │ p │ tab  │
-//        ├──────┼───┼───┼─────────────┼──────┼──────┤                                                               ├──────┼──────┼─────────┼───┼───┼──────┤
-//        │ esc  │ a │ s │      d      │  f   │  g   │                                                               │  h   │  j   │    k    │ l │ ; │ ent  │
-//        ├──────┼───┼───┼─────────────┼──────┼──────┼─────────────┬───────────────┐   ┌───────────────┬─────────────┼──────┼──────┼─────────┼───┼───┼──────┤
-//        │ lsft │ z │ x │      c      │  v   │  b   │ MO(_SYMBOL) │ MO(_FUNCTION) │   │ MO(_FUNCTION) │ MO(_NUMBER) │  n   │  m   │    ,    │ . │ / │ rsft │
-//        └──────┴───┴───┼─────────────┼──────┼──────┼─────────────┼───────────────┤   ├───────────────┼─────────────┼──────┼──────┼─────────┼───┴───┴──────┘
+//        ┌──────┬───┬───┬─────────────┬──────┬──────┐                                                               ┌──────┬──────┬─────────┬───┬───┬─────────────┐
+//        │  `   │ q │ w │      e      │  r   │  t   │                                                               │  y   │  u   │    i    │ o │ p │     tab     │
+//        ├──────┼───┼───┼─────────────┼──────┼──────┤                                                               ├──────┼──────┼─────────┼───┼───┼─────────────┤
+//        │ esc  │ a │ s │      d      │  f   │  g   │                                                               │  h   │  j   │    k    │ l │ ' │      ;      │
+//        ├──────┼───┼───┼─────────────┼──────┼──────┼─────────────┬───────────────┐   ┌───────────────┬─────────────┼──────┼──────┼─────────┼───┼───┼─────────────┤
+//        │ lsft │ z │ x │      c      │  v   │  b   │ MO(_SYMBOL) │ MO(_FUNCTION) │   │ MO(_FUNCTION) │ MO(_NUMBER) │  n   │  m   │    ,    │ . │ / │ RSFT_T(ent) │
+//        └──────┴───┴───┼─────────────┼──────┼──────┼─────────────┼───────────────┤   ├───────────────┼─────────────┼──────┼──────┼─────────┼───┴───┴─────────────┘
 //                       │ MO(_ADJUST) │ lalt │ lgui │     spc     │     lctl      │   │     rctl      │    bspc     │ rgui │ ralt │ QK_LEAD │
 //                       └─────────────┴──────┴──────┴─────────────┴───────────────┘   └───────────────┴─────────────┴──────┴──────┴─────────┘
 [_QWERTY] = LAYOUT(
-      KC_GRV  , KC_Q , KC_W , KC_E        , KC_R    , KC_T    ,                                                                 KC_Y    , KC_U    , KC_I    , KC_O   , KC_P    , KC_TAB ,
-      KC_ESC  , KC_A , KC_S , KC_D        , KC_F    , KC_G    ,                                                                 KC_H    , KC_J    , KC_K    , KC_L   , KC_SCLN , KC_ENT ,
-      KC_LSFT , KC_Z , KC_X , KC_C        , KC_V    , KC_B    , MO(_SYMBOL) , MO(_FUNCTION) ,     MO(_FUNCTION) , MO(_NUMBER) , KC_N    , KC_M    , KC_COMM , KC_DOT , KC_SLSH , KC_RSFT,
+      KC_GRV  , KC_Q , KC_W , KC_E        , KC_R    , KC_T    ,                                                                 KC_Y    , KC_U    , KC_I    , KC_O   , KC_P    , KC_TAB        ,
+      KC_ESC  , KC_A , KC_S , KC_D        , KC_F    , KC_G    ,                                                                 KC_H    , KC_J    , KC_K    , KC_L   , KC_QUOT , KC_SCLN       ,
+      KC_LSFT , KC_Z , KC_X , KC_C        , KC_V    , KC_B    , MO(_SYMBOL) , MO(_FUNCTION) ,     MO(_FUNCTION) , MO(_NUMBER) , KC_N    , KC_M    , KC_COMM , KC_DOT , KC_SLSH , RSFT_T(KC_ENT),
                               MO(_ADJUST) , KC_LALT , KC_LGUI , KC_SPC      , KC_LCTL       ,     KC_RCTL       , KC_BSPC     , KC_RGUI , KC_RALT , QK_LEAD
 ),
 
